d64a2339a37746c7d3b9fc34fae11dc7c0510b5a
Gene P. Cross <gpc.ctrlwq@gmail.com>
2024-12-21 00:44:05 +0800

    change lsp keymap
    revert <tab> to default

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,7 +5,7 @@ local cmp = require('cmp')
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	--['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	--['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<Tab>'] = cmp.mapping.confirm({ select = true }),
+--	['<Tab>'] = cmp.mapping.confirm({ select = true }),
 })
 
 lsp.setup_nvim_cmp({ mapping = cmp_mappings })
