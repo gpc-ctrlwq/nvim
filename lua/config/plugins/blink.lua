cbf8d0a9d8f918bec1431c8501539337c3e17114
Gene P. Cross <gpc.ctrlwq@gmail.com>
2024-12-23 01:04:51 +0800

    fix tab behaviour

--- lua/config/plugins/blink.lua
+++ lua/config/plugins/blink.lua
@@ -8,7 +8,17 @@ return {
     opts = {
         keymap = {
             preset = 'default',
-            ['<tab>'] = {'accept'};
+            ['<tab>'] = {
+                function(cmp)
+                    if cmp.snippet_active() then
+                        return cmp.accept()
+                    else
+                        return cmp.select_and_accept()
+                    end
+                end,
+                'snippet_forward',
+                'fallback'
+            },
         },
     }
 }
