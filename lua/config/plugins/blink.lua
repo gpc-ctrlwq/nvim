6d7a6abd85007e83a7d551877dec7a4c8271a442
Gene P. Cross <gpc.ctrlwq@gmail.com>
2025-01-07 10:26:29 +0800

    get latest blink cmp version

--- lua/config/plugins/blink.lua
+++ lua/config/plugins/blink.lua
@@ -3,7 +3,7 @@ return {
     dependencies = 'rafamadriz/friendly-snippets',
 
     -- use a release tag to download pre-built binaries
-    version = 'v0.8.*',
+    version = '*',
 
     opts = {
         keymap = {
