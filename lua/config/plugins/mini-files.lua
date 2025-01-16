705429ebf536784a57706d4dcbffc37eae493d72
Gene P. Cross <gpc.ctrlwq@gmail.com>
2025-01-17 00:40:47 +0800

    add relative line numbers to mini.files

--- lua/config/plugins/mini-files.lua
+++ lua/config/plugins/mini-files.lua
@@ -4,5 +4,15 @@ return {
     config = function()
         require('mini.files').setup()
         vim.keymap.set('n', '<leader>e', ':lua MiniFiles.open()<cr>')
+
+        -- relative line numbers
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'MiniFilesWindowUpdate',
+            callback = function(args)
+                local win = vim.wo[args.data.win_id]
+                win.number = true
+                win.relativenumber = true
+            end,
+        })
     end
 }
