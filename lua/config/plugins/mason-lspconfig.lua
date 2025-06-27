dd7eae98d36976378256772860b6ff179585dd96
Gene P. Cross <gpc.ctrlwq@gmail.com>
2025-06-27 22:30:06 +0800

    remove deprecated setup_handlers func call

--- lua/config/plugins/mason-lspconfig.lua
+++ lua/config/plugins/mason-lspconfig.lua
@@ -22,32 +22,32 @@ return {
         'williamboman/mason.nvim',
     },
     config = function()
-        require('mason-lspconfig').setup({
-            ensure_installed = { 'lua_ls', 'zls', 'ts_ls', 'clangd', 'sqlls' },
-        })
-
         local capabilities = require('cmp_nvim_lsp').default_capabilities();
 
-        require('mason-lspconfig').setup_handlers {
-            -- default handler, do not manually set up servers in lspconfig
-            function(server_name)
-                require("lspconfig")[server_name].setup { capabilities = capabilities }
-            end,
+        require('mason-lspconfig').setup({
+            ensure_installed = { 'lua_ls', 'zls', 'ts_ls', 'clangd', 'sqlls' },
 
-            -- dedicated handlers go here
-            ["lua_ls"] = function()
-                local lspconfig = require("lspconfig")
-                lspconfig.lua_ls.setup {
-                    settings = {
-                        Lua = {
-                            diagnostics = {
-                                globals = { "vim" }
+            handlers = {
+                -- default handler, do not manually set up servers in lspconfig
+                function(server_name)
+                    require("lspconfig")[server_name].setup { capabilities = capabilities }
+                end,
+
+                -- dedicated handlers go here
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim" }
+                                }
                             }
                         }
                     }
-                }
-            end,
+                end,
+            }
 
-        }
+        })
     end
 }
